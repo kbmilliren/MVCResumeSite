fddc58b50f8854b1614f4c83a5aff959ebdec0a2
Bryant Milliren <kbmilliren@northstate.net>
2015-01-27 20:22:12 -0500

    First Commit

---
+++ MVCResumeSite/MVCResumeSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCResumeSite.Startup))]
+namespace MVCResumeSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
