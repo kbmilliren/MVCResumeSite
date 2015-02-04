fc0e0758617238838d1e4cdf121b3930e602390f
Bryant Milliren <kbmilliren@northstate.net>
2015-02-03 20:55:53 -0500

    blog

--- MVCResumeSite/MVCResumeSite/Models/Post.cs
+++ MVCResumeSite/MVCResumeSite/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,9 +13,12 @@ namespace MVCResumeSite.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
+        [DisplayFormat( DataFormatString="{0:dd/MM/yy}", ApplyFormatInEditMode=true )]
         public DateTimeOffset DateCreated { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
         public DateTimeOffset DateUpdated { get; set; }
         [AllowHtml]
+        [Required]
         public string Body { get; set; }
         public string MediaUrl { get; set; }
         public bool Published { get; set; }
