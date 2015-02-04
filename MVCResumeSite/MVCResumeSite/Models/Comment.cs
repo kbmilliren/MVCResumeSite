9b9c80378ae90862053ff6efa8e7c5f3bda51c73
Bryant Milliren <kbmilliren@northstate.net>
2015-02-03 19:07:38 -0500

    Blog

---
+++ MVCResumeSite/MVCResumeSite/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCResumeSite.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset DateUpdated { get; set; }
+        public string UpdateReason { get; set; }
+        public string Body { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
---
+++ MVCResumeSite/MVCResumeSite/Models/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCResumeSite.Models
+{
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset DateUpdated { get; set; }
+        [AllowHtml]
+        public string Body { get; set; }
+        public string MediaUrl { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+    }
+
+}
